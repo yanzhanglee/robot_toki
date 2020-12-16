a4aed129f18d5df119375d466d925fcfee33192d
liyanzhang <854605104@qq.com>
2020-12-16 21:50:42 +0800

    Add function of face detection

--- js/main.js
+++ js/main.js
@@ -495,7 +495,7 @@ function speak(text, lang) {
     msg.voiceURI = 'native';
     msg.volume = 1; // 0 to 1
     msg.rate = 1.0; // 0.1 to 10
-    msg.pitch = 2; //0 to 2
+    msg.pitch = 1.5; //0 to 2
     msg.text = text;
     msg.lang = lang;
     msg.onend = function(e) {
